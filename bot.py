468941cc2244881cab964f2eab3c1bce0d00e6a0
poolitzer <github@poolitzer.eu>
2022-08-31 17:16:30 +0200

    Feat: Send error messages somewhere.

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import asyncio
+import html
+import json
 import logging
+import traceback
+
 from telegram import (
     Update,
     InlineKeyboardButton,
@@ -35,9 +39,40 @@ logging.basicConfig(
     filename="log.log",
 )
 
+logger = logging.getLogger(__name__)
+
 JOINREQUESTCHAT = -100
 MAINCHAT = -100
+DEVCHAT = 208589966
 background_tasks = set()
+
+
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Log the error and send a telegram message to notify the developer."""
+    # Log the error before we do anything else, so we can see it even if something breaks.
+    logger.error(msg="Exception while handling an update:", exc_info=context.error)
+
+    # traceback.format_exception returns the usual python message about an exception, but as a
+    # list of strings rather than a single string, so we have to join them together.
+    tb_list = traceback.format_exception(
+        None, context.error, context.error.__traceback__
+    )
+    tb_string = "".join(tb_list)
+
+    # Build the message with some markup and additional information about what happened.
+    # You might need to add some logic to deal with messages longer than the 4096 character limit.
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    message = (
+        f"An exception was raised while handling an update\n"
+        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+        "</pre>\n\n"
+        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+        f"<pre>{html.escape(tb_string)}</pre>"
+    )
+
+    # Finally, send the message
+    await context.bot.send_message(chat_id=DEVCHAT, text=message)
 
 
 def create_buttons(user_id: int):
@@ -251,6 +286,7 @@ if __name__ == "__main__":
         MessageHandler(filters.ChatType.PRIVATE, message_from_private)
     )
     application.add_handler(CallbackQueryHandler(button_callback))
+    application.add_error_handler(error_handler)
     application.run_polling(
         allowed_updates=[
             Update.MESSAGE,
